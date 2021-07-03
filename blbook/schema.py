580858772cc54776ad6227d437c29f3e06874556
Rubens Leao <rubensoleao@gmail.com>
2021-07-03 00:11:02 -0300

    Added login mutation

--- blbook/schema.py
+++ blbook/schema.py
@@ -47,7 +47,7 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user, success=True)
 
 class Mutation(graphene.ObjectType):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    login = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     register = CreateUser.Field()
